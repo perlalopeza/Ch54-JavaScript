5d827e750742c667ad1d097c16360afcfe7c09dd
Perla Lopez <perlalopez35046@gmail.com>
2025-05-16 14:34:22 -0700

    feat: sesion 16/05/25 pt.2 break, continue, Math.random

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -6,8 +6,8 @@ const myArray2 = new Array(); // []
 const myArray3 = [5]; // [5]
 const myArray4 = new Array(5); // [undefined, undefined, undefined, undefined, undefined]
 
-const myArray5 = [5,6]; // [5,6]
-const myArray6 = new Array(5,6); // [5,6]
+const myArray5 = [5, 6]; // [5,6]
+const myArray6 = new Array(5, 6); // [5,6]
 
 // ============ Iterar un  arreglo con for loop ============
 /*
@@ -18,8 +18,8 @@ const myArray6 = new Array(5,6); // [5,6]
 
 */
 const nombres = ["Juan", "Pedro", "María", "Ana"];
-for ( let index = 0; index < nombres.length; index++ ){
-    console.log( nombres[index] ); // Juan, Pedro, María, Ana
+for (let index = 0; index < nombres.length; index++) {
+    console.log(nombres[index]); // Juan, Pedro, María, Ana
 }
 
 
@@ -37,11 +37,11 @@ const colores = ["Rojo", "Azul", "Verde", "Amarillo"];
 
 for (let i = 0; i < colores.length; i++) {
     const element = colores[i];
-    console.log(element);    
+    console.log(element);
 }
 
-for ( const color of colores ) {
-    console.log( color );
+for (const color of colores) {
+    console.log(color);
 }
 
 // ---------------------- Ejercicio con for of ---------------------
@@ -55,27 +55,82 @@ const cantantes = ["Juan Gabriel", "José José", "Rocío Dúrcal", "Ana Gabriel
 const refListaCantantes = document.getElementById("cantantes-lista");
 
 
+// Imprimir en consola cada uno de los cantantes, usandor for of
+// La salida deber ser como "Juan Gabriel - José José - Rocío Dúrcal - Ana Gabriel -"
+
+const imprimirCantantes2 = (listaCantantes) => {
+    let cantantesConcatenados = "";
+    for (const cantante of listaCantantes) {
+        // cantantesConcatenados = cantantesConcatenados + cantante + " - ";
+        cantantesConcatenados += cantante + " - ";
+    }
+    return cantantesConcatenados;
+}
+console.log(imprimirCantantes2(cantantes));
+
+
+/**
+ *  Del siguiente arreglo de cantantes, mostrar en el DOM, el listado como unorder list.
+ *  const cantantes = ["Juan Gabriel", "José José", "Rocío Dúrcal", "Ana Gabriel"];
+ *  - Usar for of
+ *  - De preferencia usar una función 
+ */
+
+/* const hacerLista = (listaDeCantantes) => {
+    let lista = "";
+    for (const cantante of listaDeCantantes) {
+       lista += "<li>" + cantante + "</li>";
+    }
+    return lista;
+}
+
+const imprimirEnDom = (lista) => {
+    document.getElementById("cantantes-lista").innerHTML = hacerLista(lista);
+}
+imprimirEnDom(hacerLista(cantantes));
+ */
+
+const mostrarListaEnDOM = arr => {
+    let ul = document.getElementById('cantantes-lista');
+    let listaDesordenada = "";
+    
+    for (const cantante of arr) {
+        listaDesordenada += `<li>${cantante}</li>`;
+    }
+    ul.innerHTML = listaDesordenada;
+}
+mostrarListaEnDOM(cantantes);
+
+
+
 
 // ------------------- Uso de break en ciclos ----------------------------
 // break detiene la ejecución de la iteración en curso y termina el ciclo.
 
 let iteracion = 0;
 
-for ( ;   ;  ){
-    console.log( `Núm de iteración: ${iteracion}` ); // 0, 1, 2, 3, 4
+for (; ;) {
+    console.log(`Núm de iteración: ${iteracion}`); // 0, 1, 2, 3, 4
     iteracion++;
-    if ( iteracion === 5 ){
+    if (iteracion === 5) {
         break;
     }
 }
 
+
+
+
+
+
+
+
 // ------------------- Uso de break y label en ciclos anidados ----------------------------
 multiplicando:
-for (let i = 1; i <= 7; i++ ){
+for (let i = 1; i <= 7; i++) {
     multiplicador:
-    for (let j = 1; j <= 10 ; j++){        
-        console.log(`${i} x ${j} = ${i * j}`);             
-        if( i >= 4 ) break multiplicando;
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+        if (i >= 4) break multiplicando;
     }
 
 }
@@ -88,17 +143,17 @@ for (let i = 1; i <= 7; i++ ){
 // break: Termina completamente un bucle (for, while, switch, etc.).
 // continue: Salta la iteración actual y pasa a la siguiente sin salir del bucle.
 
-console.log(   NaN === NaN );  // false
-for (let i = 0 ; i <= 5; i++ ){
-    if ( i === 3) continue;
-    console.log("Estoy dentro del ciclo for"); 
-    console.log("Valor de i ", i ); // 0, 1, 2,  4 , 5
+console.log(NaN === NaN);  // false
+for (let i = 0; i <= 5; i++) {
+    if (i === 3) continue;
+    console.log("Estoy dentro del ciclo for");
+    console.log("Valor de i ", i); // 0, 1, 2,  4 , 5
 }
 
-for (let i = 0 ; i <= 5; i++ ){
-    if ( i !== 3){ 
-    console.log("Estoy dentro del ciclo for"); 
-    console.log("Valor de i ", i ); // 0, 1, 2,  4 , 5
+for (let i = 0; i <= 5; i++) {
+    if (i !== 3) {
+        console.log("Estoy dentro del ciclo for");
+        console.log("Valor de i ", i); // 0, 1, 2,  4 , 5
     }
 }
 
@@ -120,3 +175,41 @@ for (let i = 0 ; i <= 5; i++ ){
 */
 
 
+/*
+ Pregunta al usuario si quire que se genere su númer de la suerte.
+ Si la respuesta es "si", genera de forma aleatoria un número.
+ En cas contrario, despedirse.
+*/ 
+/* while(  confirm("¿Quieres tu número de la suerte?")  ){
+    const numeroSuerte = Math.random();
+    console.log("Tu número de la suerte es: " + numeroSuerte);
+} */
+
+
+/* 
+ Uso de Math.random();
+ Generar 5 números aleatorios.
+ Los números deben estar entre el 0.0 y 10.0(sin incluir 10.0)
+*/
+
+const generarNumerosAleatorios = (cantidad) => {
+   
+    for (let i = 0; i < cantidad; i++) {
+        const numeroAleatorio = Math.random() * 10;
+        console.log("Número aleatorio: ", numeroAleatorio); // 0.0...10.0
+    }
+    return numerosAleatorios;
+};
+generarNumerosAleatorios(5);
+
+const generarNumerosAleatorios2 = (cantidad) => {
+   
+    for (let i = 0; i < cantidad; i++) {
+        const numeroAleatorio = Math.random();
+        const escalarNumero = numeroAleatorio * 10;
+        const numerosEnteros = Math.floor(escalarNumero);
+        console.log("Número aleatorio: ", numerosEnteros); // 0.0...10.0
+    }
+    return numeroAleatorio;
+};
+generarNumerosAleatorios2(5);
